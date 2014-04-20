3e06bb396e6ff6c871c53907f1a5cd74e7d2d990
Satoshi Kado <kado@i-s-create.com>
2014-04-20 21:10:23 +0900

    Finish layout and routes

--- config/application.rb
+++ config/application.rb
@@ -1,11 +1,13 @@
-require File.expand_path('../boot', __FILE__)
 
 # Pick the frameworks you want:
 require "active_record/railtie"
 require "action_controller/railtie"
 require "action_mailer/railtie"
 require "sprockets/railtie"
+require File.expand_path('../boot', __FILE__)
+
 # require "rails/test_unit/railtie"
+
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -13,6 +15,8 @@ Bundler.require(:default, Rails.env)
 
 module SampleApp
   class Application < Rails::Application
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
